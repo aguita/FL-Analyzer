82baad1072e00478d696bce93d98db043aaf3959
Ryan Waterer <aguitadelmar@gmail.com>
2013-11-04 15:58:00 -0700

    Initial checkin

---
+++ src/functions/Arrays.java
@@ -0,0 +1,9 @@
+package functions;
+
+import java.util.ArrayList;
+
+public class Arrays {
+	
+	ArrayList<String> classList = new ArrayList<String>();
+
+}
---
+++ src/main/Driver.java
@@ -0,0 +1,12 @@
+package main;
+
+import gui.FrameMain;
+
+public class Driver {
+
+	
+	public static void main (String [] args){
+		new FrameMain();
+		
+	}
+}
