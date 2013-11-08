bbc87d01fb4335bad5fa43142f7f9f35a9448f5d
Ryan Waterer <aguitadelmar@gmail.com>
2013-11-08 16:16:07 -0700

    Quick update to give Karp the shipName

--- src/objects/ini/Ship.java
+++ src/objects/ini/Ship.java
@@ -1,5 +1,24 @@
 package objects.ini;
 
+/**
+ * 
+ * @author Ryan Waterer
+ *
+ */
 public class Ship extends Good {
+
+	private String shipName;
+
+	public String getShipName() {
+		return shipName;
+	}
+
+	public void setShipName(String shipName) {
+		this.shipName = shipName;
+	}
+
+	public void shipNameToString(String shipName) {
+		this.shipName = shipName;
+	}
 
 }
